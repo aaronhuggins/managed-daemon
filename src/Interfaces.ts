565f09b009d9af1c54273e4bb9788293aec190cf
Aaron Huggins <ahuggins@nethealthshops.com>
2020-12-03 16:42:08 -0600

    Make remaining optional args actually optional

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -2,19 +2,19 @@ export interface ServiceOptions {
   /** The name of an executable on the system PATH or an absolute path to one. */
   command: string
   /** An array of arguments to pass to the executable. */
-  args: string[]
+  args?: string[]
   /** The name of this service; used in function naming for debugging purposes. */
-  name: string
+  name?: string
   /** A number of milliseconds to pause after starting but before setting service state. */
-  startWait: number
+  startWait?: number
   /** A callback function which runs as soon as the service is ready. */
-  onReady: Function
+  onReady?: Function
   /** A callback function which runs immediately before starting the executable. */
-  onStart: Function
+  onStart?: Function
   /** A callback function which runs immediately after stopping the executable. */
-  onStop: Function
+  onStop?: Function
   /** A callback function which runs immediately after stopping the executable but before starting. */
-  onRestart: Function
+  onRestart?: Function
 }
 
 export type ServiceState = 'READY' | 'STARTED' | 'STOPPED' | 'RESTARTING' | 'UNDEFINED'
